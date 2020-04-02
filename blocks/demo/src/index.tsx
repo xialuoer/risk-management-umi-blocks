f754630f5abaeeabc8a639adeab29e06d13e817b
zhangli <72065574@vivo.xyz>
2020-04-02 11:03:29 +0800

    feat:新增一个blocks

---
+++ blocks/demo/src/index.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent, useEffect } from 'react';
+
+interface IProps {
+}
+
+const Component: FunctionComponent<IProps> = props => {
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <>
+      Demo！
+    </>
+  )
+};
+
+export default Component;
